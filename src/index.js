b15ecf836915fc12fd6a94a6586c9b331e9e8910
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2024-04-07 07:05:16 +0800

    done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
-import App from './components/App';
-
-import './reset.css';
-import './styles.css';
+import './index.css'
+import App from './components/App/App';
 
 const root = createRoot(document.querySelector('#root'));
 root.render(<App />);
